6f7c96a987f999b70512721c2b6d8aacd72a2995
Joseph Kato <joseph@jdkato.io>
2018-10-11 17:19:37 -0700

    WIP

--- docs/build.sh
+++ docs/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ba="https://github.com/gohugoio/hugo/releases/download/"
-bb="v0.49/hugo_0.49_${OS}-64bit.tar.gz"
+bb="v0.49/hugo_0.49_Linux-64bit.tar.gz"
 bc=$ba$bb
 
 cd docs && curl -sL $bc | tar xz && ./hugo
